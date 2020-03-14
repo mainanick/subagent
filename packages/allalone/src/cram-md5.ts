57085dc91f7d6b7e00c78756cf308ace34880fbd
Maina Nick <nick.maina1@gmail.com>
2020-03-14 18:22:03 +0300

    lol

--- packages/allalone/src/cram-md5.ts
+++ packages/allalone/src/cram-md5.ts
@@ -1,9 +1,5 @@
 import crypto from "crypto";
-export function response(
-  username: string,
-  password: string,
-  challenge: string
-) {
+export function response(username: string, password: string, challenge: string) {
   const chlg = Buffer.from(challenge, "base64").toString("ascii"); // Challenge
   const hmac = crypto.createHmac("md5", password);
   hmac.update(chlg);
